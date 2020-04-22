5cfe3733455d5bd52773fdc6f567b3818b443e30
zhangwd <zhangwd@corp.21cn.com>
2020-04-22 09:54:09 +0800

    项目新建

---
+++ drawPro/src/pages/mainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    <span>hello</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
